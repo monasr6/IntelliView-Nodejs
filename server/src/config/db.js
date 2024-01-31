064b898950559a33bc0bb4cd9e63e72b54043bf8
Mohamed Nasr <mahamednasr6@gmail.com>
2024-01-31 14:07:26 +0200

    Dockrize the app restructure files

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 const catchAsync = require('../utils/catchAsync');
 
 exports.connectDB = catchAsync(async () => {
-  await mongoose.connect(process.env.DATABASE, {
-    // .connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-  });
+  await mongoose.connect(process.env.DATABASE, {});
   console.log('DB connection successful!');
 });
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -30,7 +30,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   res.cookie('jwt', token, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
     ),
     // secure: true,
     httpOnly: true,
